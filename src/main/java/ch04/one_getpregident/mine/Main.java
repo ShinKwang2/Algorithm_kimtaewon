2d9799324a23ddb80d3d1ce001a485a69f3dcbad
kwang <shinkwang@yonsei.ac.kr>
2022-05-31 13:10:52 +0900

    Chapter 6_6

--- src/main/java/ch04/one_getpregident/mine/Main.java
+++ src/main/java/ch04/one_getpregident/mine/Main.java
@@ -1,11 +1,33 @@
 package ch04.one_getpregident.mine;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
-    public char solution(int n, String str) {
+    public Character solution(int n, String str) {
         char answer = ' ';
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char x : str.toCharArray()) {
+            if (map.containsKey(x)) {
+                map.put(x, map.get(x) + 1);
+            }
+            else {
+                map.put(x, 1);
+            }
+        }
+
+        int max = 0;
+        for (Character c : map.keySet()) {
+            if (map.get(c) > max) {
+                max = map.get(c);
+                answer = c;
+            }
+        }
+
+
 
         return answer;
     }
